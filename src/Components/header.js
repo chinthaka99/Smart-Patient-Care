8da242cf7f36e905ca6788fb8d805c0c9df9ad24
chinthaka99 <dilhanchinthaka99@gmail.com>
2023-03-22 01:00:54 +0530

    second commit

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import Logo from '../Img/Logo.png';
-import './Styles/header.css';
+import React, { useState } from 'react'
+import Logo from '../Assesst/img/Logo.png';
+import '../Assesst/style/header.css';
 
 export default function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <div>
-        <nav className='header'>
-            <img src={Logo} alt='Logo' className='header-logo' />
-            <ul className='header-list'>
-                <li className='header-list-item'>My Profile</li>
-                <li className='header-list-item'>Service</li>
-                <li className='header-list-item'>About Us</li>
-            </ul>
-            <button className='signup-button'>Sign Up</button>
-        </nav>
+      <nav className='header'>
+        <img src={Logo} alt='Logo' className='header-logo' />
+        <button className='hamburger' onClick={() => setShowMenu(!showMenu)}>☰</button>
+        <ul className={`header-list ${showMenu ? "show-menu" : ""}`}>
+          <li className='header-list-item'>My Profile</li>
+          <li className='header-list-item'>Service</li>
+          <li className='header-list-item'>About Us</li>
+        </ul>
+        <button className='signup-button'>Sign Up</button>
+      </nav>
     </div>
   )
 }

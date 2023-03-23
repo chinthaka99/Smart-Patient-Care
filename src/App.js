46aa5208dad1e5b6d58f88b1823730c6a599b9a8
chinthaka99 <dilhanchinthaka99@gmail.com>
2023-03-23 18:59:37 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/header';
+import LoginForm from './Components/loginform';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <LoginForm />
     </div>
   );
 }
---
+++ src/Components/loginform.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../Assesst/style/loginform.css'
+
+export default function LoginForm() {
+  return (
+    <div>
+        <div className='login-container'>
+            <div className='login-background-box'>
+                <div className='link-signup'>
+                    <p>Don't have and Account?</p>
+                    <button className='sign-up-button'>SIGN UP</button>
+                </div>
+                <div className='login-heading'>
+                    <span>Welcome to InteliTrend</span>
+                    <p>Log in to your account</p>
+                </div>
+
+                <form className='login-form'>
+                    <div className='login-item'>
+                        <label>Email: </label>
+                        <input type='email' placeholder='youremail@gmail.com'></input>
+                    </div>
+                    <div className='login-item'>
+                        <label>Password: </label>
+                        <input type='password' placeholder='**************'></input>
+                    </div>  
+                    <button className='login-button'>LOG IN</button>
+                </form> 
+               
+            </div>
+        </div>
+    </div>
+  )
+}
